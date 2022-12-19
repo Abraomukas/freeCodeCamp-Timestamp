61f09a880aaf8bc5bf5c0b0466b129fe2699f18c
Abraomukas <abraomukas@gmail.com>
2022-12-19 01:25:53 +0000

    feat: add parseMonth()
    add function to transform months from a number to readable text

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ function parseDateToUTC(dateString) {
 }
 
 /**
- * Parses days input from Number to readable text format
+ * Parses days input from a number to readable text
  * @param dayInput
  * @returns the short and readable version of @param
  */
@@ -100,6 +100,40 @@ function parseDay(dayInput) {
 			return 'Sat, ';
 		case 7:
 			return 'Sun, ';
+	}
+}
+
+/**
+ * Parses months input from a number to readable text
+ * @param monthInput
+ * @returns the short and readable version of @param
+ */
+function parseMonth(monthInput) {
+	switch (monthInput) {
+		case 1:
+			return ' Jan ';
+		case 2:
+			return ' Feb ';
+		case 3:
+			return ' Mar ';
+		case 4:
+			return ' Apr ';
+		case 5:
+			return ' May ';
+		case 6:
+			return ' Jun ';
+		case 7:
+			return ' Jul';
+		case 8:
+			return ' Aug ';
+		case 9:
+			return ' Sep ';
+		case 10:
+			return ' Oct ';
+		case 11:
+			return ' Nov ';
+		case 12:
+			return ' Dec ';
 	}
 }
 
